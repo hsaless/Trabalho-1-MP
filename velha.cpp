745f83d8151e32a565142055a38058e5d0b69634
Henrique <hcrsaless@gmail.com>
2024-12-02 02:07:33 -0300

    Passa X ganhando por diagonal

--- velha.cpp
+++ velha.cpp
@@ -20,6 +20,7 @@
  * \param velha Matriz representando o estado do jogo.
  * \return 1 se X venceu, 2 se O venceu, 0 se não há vencedor.
  */
+
 int verifica_coluna(int velha[3][3]) {
   for (int j = 0; j < 3; j++) {
     if (velha[0][j] == velha[1][j] && velha[1][j] == velha[2][j]) {
@@ -39,7 +40,6 @@ int verifica_coluna(int velha[3][3]) {
  * \return 1 se X venceu, 2 se O venceu, 0 se não há vencedor.
  */
 
-
 int verifica_linha(int velha[3][3]) {
   for (int i = 0; i < 3; i++) {
     if (velha[i][0] == velha[i][1] && velha[i][1] == velha[i][2]) {
@@ -54,6 +54,26 @@ int verifica_linha(int velha[3][3]) {
   return 0;  // Sem vencedor
 }
 
+/**
+ * \brief Verifica se há vencedor na diagonal.
+ * \param velha Matriz representando o estado do jogo.
+ * \return 1 se X venceu, 2 se O venceu, 0 se não há vencedor.
+ */
+
+int verifica_diagonal(int velha[3][3]) {
+  if (velha[0][0] == velha[1][1] && velha[1][1] == velha[2][2]) {
+    if (velha[0][0] == 1) {
+      return 1;  // X venceu
+    }
+  }
+  if (velha[2][0] == velha[1][1] && velha[1][1] == velha[0][2]) {
+    if (velha[0][2] == 1) {
+      return 1;  // X venceu
+    }
+  }
+  return 0;  // Sem vencedor
+}
+
 int VerificaVelha(int velha[3][3]) {
   int vencedor = verifica_coluna(velha);
   if (vencedor != 0) {
@@ -63,7 +83,15 @@ int VerificaVelha(int velha[3][3]) {
   if (vencedor !=  0) {
     return vencedor;
   }
+  vencedor = verifica_diagonal(velha);
+  if (vencedor != 0) {
+    return vencedor;
+  }
   return 0;
 }
+
+
+
+
 
 
--- velha.hpp
+++ velha.hpp
@@ -1,3 +1,4 @@
 int VerificaVelha(int velha[3][3]); 
 int verifica_coluna(int velha[3][3]);
 int verifica_linha(int velha[3][3]);
+int verifica_diagonal(int velha[3][3]);
